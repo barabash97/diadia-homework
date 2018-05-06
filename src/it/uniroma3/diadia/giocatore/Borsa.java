ad8b7c790d084599c335588cf87a99d680eb2a14
Volodymyr Barabash <barabash97@gmail.com>
2018-05-06 17:52:14 +0200

    refactoring

--- src/it/uniroma3/diadia/giocatore/Borsa.java
+++ src/it/uniroma3/diadia/giocatore/Borsa.java
@@ -1,5 +1,8 @@
 package it.uniroma3.diadia.giocatore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 /**
@@ -11,8 +14,8 @@ import it.uniroma3.diadia.attrezzi.Attrezzo;
 public class Borsa {
 	public final static int DEFAULT_PESO_MAX_BORSA = 10;
 	public final static int DEFAULT_NUMERO_MAX_ATTREZZI = 10;
-	private Attrezzo[] attrezzi; //Array di attrezzi
-	private int numeroAttrezzi; //contatore di attrezzi
+	private List<Attrezzo> attrezzi; // Array di attrezzi
+	private int numeroAttrezzi; // contatore di attrezzi
 	private int pesoMax; // peso massimo della borsa
 
 	/**
@@ -31,9 +34,9 @@ public class Borsa {
 	 * @param pesoMax
 	 */
 	public Borsa(int pesoMax) {
-		this.pesoMax = pesoMax; 
-		this.attrezzi = new Attrezzo[DEFAULT_NUMERO_MAX_ATTREZZI]; // speriamo che bastino...
+		this.pesoMax = pesoMax;
 		this.numeroAttrezzi = 0;
+		this.attrezzi = new ArrayList<>();
 	}
 
 	/**
@@ -47,144 +50,166 @@ public class Borsa {
 			return false;
 		if (this.numeroAttrezzi == DEFAULT_NUMERO_MAX_ATTREZZI)
 			return false;
-		this.attrezzi[this.numeroAttrezzi] = attrezzo;
+		this.attrezzi.add(attrezzo);
 		this.numeroAttrezzi++;
 		return true;
 	}
-	
+
 	/**
 	 * Gettere peso massimo
+	 * 
 	 * @return
 	 */
 	public int getPesoMax() {
 		return pesoMax;
 	}
-	
+
 	/**
 	 * Restitiusce il riferimento all'oggetto Attrezzo
-	 * @param nomeAttrezzo stringa nome dell'attrezzo
-	 * @return se presente restituisce il riferimento all'oggetto Attrezzo, altrimenti null
+	 * 
+	 * @param nomeAttrezzo
+	 *            stringa nome dell'attrezzo
+	 * @return se presente restituisce il riferimento all'oggetto Attrezzo,
+	 *         altrimenti null
 	 */
 	public Attrezzo getAttrezzo(String nomeAttrezzo) {
 		Attrezzo a = null;
-		for (int i = 0; i < this.numeroAttrezzi; i++)
-			if (this.attrezzi[i].getNome().equals(nomeAttrezzo))
-				a = attrezzi[i];
+
+		for (Attrezzo attrezzo : this.attrezzi) {
+			if (attrezzo.getNome().equals(nomeAttrezzo))
+				a = attrezzo;
+		}
+
 		return a;
 	}
-	
+
 	/**
 	 * Getter peso della borsa
+	 * 
 	 * @return intero
 	 */
 	public int getPeso() {
 		int peso = 0;
-		for (int i = 0; i < this.numeroAttrezzi; i++)
-			if(this.attrezzi[i] != null) {
-				peso += this.attrezzi[i].getPeso();
+
+		for (Attrezzo attrezzo : this.attrezzi) {
+			if(attrezzo != null) {
+				peso += attrezzo.getPeso();
 			}
-			
+		}
+
 		return peso;
 	}
-	
+
 	/**
 	 * Controllo se la borsa è vuota
+	 * 
 	 * @return booleano
 	 */
 	public boolean isEmpty() {
 		return this.numeroAttrezzi == 0;
 	}
-	
+
 	/**
 	 * Controllo se attrezzo è presente nella borsa
-	 * @param nomeAttrezzo nome stringa dell'attrezzo
+	 * 
+	 * @param nomeAttrezzo
+	 *            nome stringa dell'attrezzo
 	 * @return true se la borsa ha questo attrezzo, altrimenti false
 	 */
 	public boolean hasAttrezzo(String nomeAttrezzo) {
 		return this.getAttrezzo(nomeAttrezzo) != null;
 	}
-	
+
 	/**
 	 * Rimuovere un attrezzo dalla borsa
-	 * @param nomeAttrezzo nome stringa dell'attrezzo
+	 * 
+	 * @param nomeAttrezzo
+	 *            nome stringa dell'attrezzo
 	 * @return restituisce true se l'attrezzo è stato rimosso, altrimenti false
 	 */
 	public boolean removeAttrezzo(String nomeAttrezzo) {
-		
-		int i;
-		
-		for(i = 0; i < this.numeroAttrezzi; i++) {
-			if(this.attrezzi[i].getNome().equals(nomeAttrezzo)) {
-				this.attrezzi[i] = this.attrezzi[this.numeroAttrezzi - 1];
-				this.attrezzi[this.numeroAttrezzi - 1] = null;
-				this.decrementaNumeroAttrezzi();
+
+		for (Attrezzo attrezzo : this.attrezzi) {
+			if (attrezzo.getNome().equals(nomeAttrezzo)) {
+				this.attrezzi.remove(attrezzo);
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Restituisce la stringa con il contenuto della borsa
 	 */
 	public String toString() {
 		StringBuilder s = new StringBuilder();
+
 		if (!this.isEmpty()) {
+
 			s.append("Contenuto borsa (" + this.getPeso() + "kg/" + this.getPesoMax() + "kg): ");
-			for (int i = 0; i < this.numeroAttrezzi; i++)
-				s.append(attrezzi[i].toString() + " ");
+
+			for (Attrezzo attrezzo : this.attrezzi) {
+				s.append(attrezzo.toString() + " ");
+			}
+
 		} else
 			s.append("Borsa vuota");
+
 		s.append("\nPeso della borsa: " + this.getPeso() + "kg");
 		return s.toString();
 	}
-	
+
 	/**
 	 * Decrementa il numero di attrezzi nella borsa
 	 */
 	public void decrementaNumeroAttrezzi() {
 		this.numeroAttrezzi--;
 	}
-	
+
 	/**
 	 * Controlla se la borsa è piena
+	 * 
 	 * @return
 	 */
 	public boolean isPieno() {
-		return ((this.getNumeroAttrezzi() == DEFAULT_NUMERO_MAX_ATTREZZI) || (this.getPeso() == DEFAULT_PESO_MAX_BORSA));
+		return ((this.getNumeroAttrezzi() == DEFAULT_NUMERO_MAX_ATTREZZI)
+				|| (this.getPeso() == DEFAULT_PESO_MAX_BORSA));
 	}
-	
+
 	/**
 	 * Il peso rimasto della borsa è sufficiente per aggiungere attrezzo
+	 * 
 	 * @param attrezzo
 	 * @return ritorna true se peso è sufficiente, altrimenti false
 	 */
 	public boolean pesoSufficientePerAggiungereAttrezzo(Attrezzo attrezzo) {
 		return ((this.getPeso() + attrezzo.getPeso()) <= this.getPesoMax());
 	}
-	
+
 	/**
-	 * Controllo se è possibile aggiungere attrezzo nella borsa 
+	 * Controllo se è possibile aggiungere attrezzo nella borsa
+	 * 
 	 * @param attrezzo
 	 * @return
 	 */
 	public boolean possibileAggiungereAttrezzo(Attrezzo attrezzo) {
-		
-		if(attrezzo == null) {
+
+		if (attrezzo == null) {
 			return false;
 		}
-		
-		if(!this.isPieno() && this.pesoSufficientePerAggiungereAttrezzo(attrezzo)) {
+
+		if (!this.isPieno() && this.pesoSufficientePerAggiungereAttrezzo(attrezzo)) {
 			return true;
 		} else {
 			return false;
 		}
-		
+
 	}
 
 	/**
 	 * Getter numero attrezzi nella borsa
+	 * 
 	 * @return
 	 */
 	public int getNumeroAttrezzi() {
@@ -193,11 +218,11 @@ public class Borsa {
 
 	/**
 	 * Setter numero attrezzi nella borsa
+	 * 
 	 * @param numeroAttrezzi
 	 */
 	public void setNumeroAttrezzi(int numeroAttrezzi) {
 		this.numeroAttrezzi = numeroAttrezzi;
 	}
-	
-	
+
 }
