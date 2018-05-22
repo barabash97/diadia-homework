b93c2a591ff4ae8cfb6fed145178621950c299d6
Volodymyr Barabash <barabash97@gmail.com>
2018-05-22 19:44:44 +0200

    refactoring

--- src/it/uniroma3/diadia/ambienti/StanzaBuia.java
+++ src/it/uniroma3/diadia/ambienti/StanzaBuia.java
@@ -3,14 +3,15 @@ package it.uniroma3.diadia.ambienti;
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 public class StanzaBuia extends Stanza {
-	
+
 	private String nomeParticolare;
 	final public static String SLOGAN = "Qui c'è un buio pesto";
 	final private static String ATTREZZO_PARTICOLARE_DEFAULT = "lanterna";
+
 	public StanzaBuia(String nome) {
 		this(nome, ATTREZZO_PARTICOLARE_DEFAULT);
 	}
-	
+
 	public StanzaBuia(String nome, String nomeAttrezzoParticolare) {
 		super(nome);
 		this.setNomeParticolare(nomeAttrezzoParticolare);
@@ -24,7 +25,8 @@ public class StanzaBuia extends Stanza {
 	}
 
 	/**
-	 * @param nomeParticolare the nomeParticolare to set
+	 * @param nomeParticolare
+	 *            the nomeParticolare to set
 	 */
 	public void setNomeParticolare(String nomeParticolare) {
 		this.nomeParticolare = nomeParticolare;
@@ -32,17 +34,14 @@ public class StanzaBuia extends Stanza {
 
 	@Override
 	public String getDescrizione() {
-		String str = "";
+
 		Attrezzo a = this.getAttrezzo(this.nomeParticolare);
-		if(a == null) {
-			str = SLOGAN;
+
+		if (a == null) {
+			return SLOGAN;
 		} else {
-			str = a.toString();
+			return a.toString();
 		}
-		return str;
 	}
 
-	
-	
-	
 }
