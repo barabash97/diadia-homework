4c1ef3708b9746d1d6402714359267c9276f0a79
Volodymyr Barabash <barabash97@gmail.com>
2018-06-11 09:33:22 +0200

    new changes

--- test/it/uniroma3/diadia/comandi/FabbricaDiComandiFisarmonicaTest.java
+++ test/it/uniroma3/diadia/comandi/FabbricaDiComandiFisarmonicaTest.java
@@ -5,9 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import it.uniroma3.diadia.Partita;
-import it.uniroma3.diadia.attrezzi.Attrezzo;
-
 public class FabbricaDiComandiFisarmonicaTest {
 
 	private FabbricaDiComandiRiflessiva fabbricaComandi;
