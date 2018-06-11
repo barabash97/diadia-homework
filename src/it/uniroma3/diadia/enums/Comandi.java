f90baf138c664e5a501af36518dbfc1878da9a5f
Volodymyr Barabash <barabash97@gmail.com>
2018-06-11 09:52:18 +0200

    new changes

--- src/it/uniroma3/diadia/enums/Comandi.java
+++ src/it/uniroma3/diadia/enums/Comandi.java
@@ -1,5 +1,5 @@
 package it.uniroma3.diadia.enums;
 
 public enum Comandi {
-	VAI, AIUTO, FINE, PRENDI, POSA, GUARDA, SALUTA, INTERAGISCI
+	VAI, AIUTO, FINE, PRENDI, POSA, GUARDA, SALUTA, INTERAGISCI, REGALA
 }
